064297cf9f7491c588c6481297414a411df5b801
Reza Molaei <R.Molaei@mail.com>
2023-09-16 16:40:43 +0330

    Some modifications were done.

--- help/packages_documents.cmd
+++ help/packages_documents.cmd
@@ -1,5 +1,13 @@
+@REM Package list:
 texdoc arabxetex
+@REM arabxetex used in .\lib\beliefs.sty
 texdoc quran
+@REM quran used in .\lib\beliefs.sty
 texdoc xspace
+@REM xspace used in .\lib\directives.sty
 texdoc etoolbox
+@REM etoolbox used in .\lib\directives.sty
 texdoc xepersian
+@REM xepersian used in .\lib\pfontsettings.sty
+texdoc microtype
+@REM microtype used in .\lib\commonPackages.sty
--- help/packages_documents.sh
+++ help/packages_documents.sh
@@ -1,5 +1,13 @@
+# Package list:
 texdoc arabxetex
+# arabxetex used in .\lib\beliefs.sty
 texdoc quran
+# quran used in .\lib\beliefs.sty
 texdoc xspace
+# xspace used in .\lib\directives.sty
 texdoc etoolbox
+# etoolbox used in .\lib\directives.sty
 texdoc xepersian
+# xepersian used in .\lib\pfontsettings.sty
+texdoc microtype
+# microtype used in .\lib\commonPackages.sty
