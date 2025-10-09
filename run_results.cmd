b3dede4746caaebcb1cab72dc87917d6bf3b3b67
Reza Molaei <R.Molaei@mail.com>
2025-10-09 18:13:34 +0330

    * Acronym feature was developed. * The run_results scripts were modified for Persian outputs.

--- run_results.cmd
+++ run_results.cmd
@@ -1,7 +1,9 @@
+chcp 65001
 set name_var=Hassan_Zirek
+set name_var_Persian=حسن_زیرک
 set date_var=%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%
 copy .\CL\CL.pdf .\Results\%name_var%_%date_var%_CL.pdf
 copy .\CV\CV_English.pdf .\Results\%name_var%_CV.pdf
-copy .\CV\CV_Persian.pdf .\Results\%name_var%_CV_Persian.pdf
+copy .\CV\CV_Persian.pdf .\Results\%name_var_Persian%_شرح_فعالیت.pdf
 copy .\Resume\Resume_English.pdf .\Results\%name_var%_Resume.pdf
-copy .\Resume\Resume_Persian.pdf .\Results\%name_var%_Resume_Persian.pdf
+copy .\Resume\Resume_Persian.pdf .\Results\%name_var_Persian%_خلاصه_فعالیت.pdf
--- run_results.sh
+++ run_results.sh
@@ -1,8 +1,9 @@
 name_var=Hassan_Zirek
+name_var_Persian=حسن_زیرک
 date_var=$(date +%F)
 date_var=${date_var:0:4}${date_var:5:2}${date_var:8:2}
 cp ./CL/CL.pdf ./Results/${name_var}_${date_var}_CL.pdf
 cp ./CV/CV_English.pdf ./Results/${name_var}_CV.pdf
-cp ./CV/CV_Persian.pdf ./Results/${name_var}_CV_Persian.pdf
+cp ./CV/CV_Persian.pdf ./Results/${name_var_Persian}_شرح_فعالیت.pdf
 cp ./Resume/Resume_English.pdf ./Results/${name_var}_Resume.pdf
-cp ./Resume/Resume_Persian.pdf ./Results/${name_var}_Resume_Persian.pdf
+cp ./Resume/Resume_Persian.pdf ./Results/${name_var_Persian}_خلاصه_فعالیت.pdf
