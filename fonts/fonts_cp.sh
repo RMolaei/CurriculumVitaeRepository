cebd86d7cc0844f75ff04e7b11503809a93be074
Reza Molaei <R.Molaei@mail.com>
2026-05-29 14:42:40 +0100

    * The .gitattributes script was developed. * The CV files were modified. * Abbreviation and Acronym features were fixed. * Project was compiled by TeX Live 2024 on Ubuntu 20.04 LTS * Project was compiled by TeX Live 2024 on Windows 10

---
+++ fonts/fonts_cp.sh
@@ -0,0 +1,10 @@
+
+sudo cp /media/molaei/Data/RezaMolaeiCV/CurriculumVitaeRepository/fonts/ArabicTypesetting/arabtype.ttf /usr/local/share/fonts
+
+sudo cp /media/molaei/Data/RezaMolaeiCV/CurriculumVitaeRepository/fonts/BMitra/BMitra.ttf     /usr/local/share/fonts
+sudo cp /media/molaei/Data/RezaMolaeiCV/CurriculumVitaeRepository/fonts/BMitra/BMitraBold.ttf /usr/local/share/fonts
+
+sudo cp /media/molaei/Data/RezaMolaeiCV/CurriculumVitaeRepository/fonts/TimesNewerRoman/times.ttf   /usr/local/share/fonts
+sudo cp /media/molaei/Data/RezaMolaeiCV/CurriculumVitaeRepository/fonts/TimesNewerRoman/timesbd.ttf /usr/local/share/fonts
+sudo cp /media/molaei/Data/RezaMolaeiCV/CurriculumVitaeRepository/fonts/TimesNewerRoman/timesbi.ttf /usr/local/share/fonts
+sudo cp /media/molaei/Data/RezaMolaeiCV/CurriculumVitaeRepository/fonts/TimesNewerRoman/timesi.ttf  /usr/local/share/fonts
